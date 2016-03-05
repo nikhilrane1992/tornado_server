bade9eea3a991604fbbbe6dc0a7d8dab3ec66a75
Nikhil Rane <nikhilrane1992@gamil.com>
2016-03-05 13:59:34 +0530

    added function for get json data in angular controller

--- static/js/custom.js
+++ static/js/custom.js
@@ -5,8 +5,45 @@ angular.module("testApp", ['ui.bootstrap', 'angular-loading-bar'])
 .controller("testCtrl", function($scope,$http){
 
     
+	// ------------------ websocket starts--------------------------
+	var WebSocketTest = function() {
+
+		if ("WebSocket" in window){
+			console.log("WebSocket is supported by your Browser!");
+
+			// Let us open a web socket
+			var ws = new WebSocket("ws://localhost:8888/tornado/webSocket/get_json/");
+
+			ws.onopen = function()
+			{
+				// Web Socket is connected, send data using send()
+				ws.send("This message is sent by Javascript!!");
+				console.log("Message is sent to Tornado...");
+			};
+
+			ws.onmessage = function (evt)
+			{
+				var received_msg = JSON.parse(evt.data);
+				console.log(received_msg);
+			};
+
+			ws.onclose = function(){
+				// websocket is closed.
+				console.log("Connection is closed...");
+			};
+		}else{
+			// The browser doesn't support WebSocket
+			console.log("WebSocket NOT supported by your Browser!");
+		}
+	};
+
+// ---------------------- websocket ends----------------------
+
+
+
+
     var init = function(){
-        
+        WebSocketTest();
     }
     init();
 
