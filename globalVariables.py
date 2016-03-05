6933b5d04e595fe1a901720ca54543de0458ccbd
Nikhil Rane <nikhilrane1992@gamil.com>
2016-03-05 13:26:27 +0530

    added .gitignore file

---
+++ globalVariables.py
@@ -0,0 +1,4 @@
+
+# storing client types along with individual clients in dictionary..
+clientTypesAndClients = {"jsonTestClient" : [] }
+
--- tornado_server.py
+++ tornado_server.py
@@ -1,15 +1,53 @@
 import tornado.ioloop
 import tornado.web
+import tornado.websocket
 
 PORT = 8888
 
-class MainHandler(tornado.web.RequestHandler):
-    def get(self):
-        self.write("Hello, world")
+from globalVariables import clientTypesAndClients
+
+class GetJsonHandler(tornado.websocket.WebSocketHandler):
+	def check_origin(self, origin):
+		return True
+
+	def open(self, *args):
+		clientTypesAndClients["jsonTestClient"].append(self)
+		print "CONNECTION AT NEW ORDERS TABLE WEBSOCKET SERVER IS OPEN!!!!"
+
+	def on_message(self, message):
+		print "NEW ORDERS TABLE SERVER RECEIVED A MESSAGE!!!!!!!!!!!!!!!!! : %s" % (message)
+
+	def json_test(self, message):
+		self.write_message(message)
+
+	def on_close(self):
+		clientTypesAndClients["jsonTestClient"].remove(self)
+
+class JsonTestHandler(tornado.web.RequestHandler):
+    @tornado.web.asynchronous
+    def post(self):
+        data = self.request.body
+
+        if data != None:
+            print "DATA RECEIVED BY ORDER PLACED HANDLER"
+            print str(data)
+        else:
+            data = {'status':False,'validation':'No data found...'}
+            data = json.dumps(data);
+            print "DATA NOT RECEIVED"
+            print data
+
+        for jsonTestClent in clientTypesAndClients["jsonTestClient"]:
+            jsonTestClent.json_test(data);
+        self.finish()
+
+    def on_message(self, message):
+        print "NEW ORDERS TABLE SERVER RECEIVED A MESSAGE!!!!!!!!!!!!!!!!! : %s" % (message)
 
 def make_app():
     return tornado.web.Application([
         (r"/", MainHandler),
+        (r'/tornado/webSocket/json_test/', JsonTestHandler),
     ])
 
 if __name__ == "__main__":
